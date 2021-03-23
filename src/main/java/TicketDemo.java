2cb421a73c0e429698973e4d8f190e9f15ed134e
“czpted” <czpted@gmail.com>
2021-03-23 21:52:33 +0800

    卖票程序——Thread形式的同步代码块

---
+++ src/main/java/TicketDemo.java
@@ -0,0 +1,38 @@
+
+class MyTicket extends Thread{
+
+    static int number = 100;
+    static Object o = new Object();
+
+    @Override
+    public void run() {
+        while (true){
+            synchronized (o){
+                if (number > 0){
+                    System.out.println(this.getName() + "卖出第" +number+"张票");
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+
+
+}
+
+
+public class TicketDemo {
+    public static void main(String[] args){
+        MyTicket myTicket1 = new MyTicket();
+        MyTicket myTicket2 = new MyTicket();
+        MyTicket myTicket3 = new MyTicket();
+        myTicket1.setName("窗口一");
+        myTicket2.setName("窗口二");
+        myTicket3.setName("窗口三");
+        myTicket1.start();
+        myTicket2.start();
+        myTicket3.start();
+
+
+    }
+}
