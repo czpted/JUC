862a825e99e0cd4ee557fef369fa2179eff69746
“czpted” <czpted@gmail.com>
2021-03-23 20:30:58 +0800

    线程的第二种创建方式——继承Runnable接口

---
+++ src/main/java/RunnableTest.java
@@ -0,0 +1,17 @@
+class MyRunnable implements Runnable{
+    public void run() {
+        for (int i = 0; i<100; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class RunnableTest{
+    public static void main (String[] args){
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+    }
+}
