103a21a5b9199e10a55c41dff1f3adc3f69ad5e3
“czpted” <czpted@gmail.com>
2021-03-23 20:24:09 +0800

    线程的第一种创建方式——继承Thread类

---
+++ src/main/java/ThreadTest.java
@@ -0,0 +1,18 @@
+class MyThread extends Thread{
+    public MyThread(){}
+    @Override
+    public void run() {
+        for(int i = 0; i<100; i++){
+            if(i%2 ==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class ThreadTest {
+    public static void main(String[] args){
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
